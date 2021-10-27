129e9b01d00a1bba5c72a87868dc0a76cb071f78
Emlyn Bottomley <bottomley.daniel.e@dol.gov>
2021-10-27 17:18:44 -0400

    add soc_crosswalk function

--- R/onet_crosswalk_function.R
+++ R/onet_crosswalk_function.R
@@ -1,3 +1,113 @@
+soc_crosswalk <- function(df, occupation_column, soc_schema = 'OES_2019_Estimates_Code', keep_details = TRUE) {
+
+  occupation_column <- enquo(occupation_column)
+
+  #This block sets the target schema for subsequent crosswalking
+  if (soc_schema == 'OES_2019_Estimates_Code') {
+    SOC_Crosswalk <- SOC_Crosswalk %>%
+      mutate(crosswalked_code = OES_2019_Estimates_Code,
+             crosswalked_title = OES_2019_Estimates_Title,
+             target_schema = soc_schema)
+  } else if (soc_schema == 'SOC_2018_Code') {
+    SOC_Crosswalk <- SOC_Crosswalk %>%
+      mutate(crosswalked_code = SOC_2018_Code,
+             crosswalked_title = SOC_2018_Title,
+             target_schema = soc_schema)
+  } else if (soc_schema == 'OES_2018_Estimates_Code') {
+    SOC_Crosswalk <- SOC_Crosswalk %>%
+      mutate(crosswalked_code = OES_2018_Estimates_Code,
+             crosswalked_title = OES_2018_Estimates_Title,
+             target_schema = soc_schema)
+  } else if (soc_schema == 'SOC_2010_Code') {
+    SOC_Crosswalk <- SOC_Crosswalk %>%
+      mutate(crosswalked_code = SOC_2010_Code,
+             crosswalked_title = SOC_2010_Title,
+             target_schema = soc_schema)
+  } else {
+    stop('soc_schema must be one of the following options:
+         OES_2019_Estimates_Code
+         SOC_2018_Code
+         OES_2018_Estimates_Code
+         SOC_2010_Code', call. = FALSE)
+  }
+
+  df_cols <- c(names(df),'crosswalked_code', 'crosswalked_title', 'target_schema', 'source_schema')
+
+  # Test whether a SOC code in the use input occupation column is a valid OES_2019_Estimates_Code.
+  # If it is the script will join to the crosswalk on the first valid match.
+  by <- setNames('OES_2019_Estimates_Code', quo_name(occupation_column))
+
+  df_2019_estimate <- df %>%
+    filter(!!occupation_column %in% SOC_Crosswalk$OES_2019_Estimates_Code) %>%
+    left_join(SOC_Crosswalk %>% distinct(OES_2019_Estimates_Code, .keep_all = TRUE),
+              by=by) %>%
+    mutate(source_schema = 'OES_2019_Estimates_Code') %>%
+    select(df_cols)
+
+  by <- setNames('SOC_2018_Code', quo_name(occupation_column))
+
+  df_2018 <- df %>%
+    filter(!(!!occupation_column %in% SOC_Crosswalk$OES_2019_Estimates_Code),
+           !!occupation_column %in% SOC_Crosswalk$SOC_2018_Code) %>%
+    left_join(SOC_Crosswalk %>% distinct(SOC_2018_Code, .keep_all = TRUE),
+              by=by) %>%
+    mutate(source_schema = 'SOC_2018_Code') %>%
+    select(df_cols)
+
+  by <- setNames('OES_2018_Estimates_Code', quo_name(occupation_column))
+
+  df_2018_estimate <- df %>%
+    filter(!(!!occupation_column %in% SOC_Crosswalk$OES_2019_Estimates_Code),
+           !(!!occupation_column %in% SOC_Crosswalk$SOC_2018_Code),
+           !!occupation_column %in% SOC_Crosswalk$OES_2018_Estimates_Code) %>%
+    left_join(SOC_Crosswalk %>% distinct(OES_2018_Estimates_Code, .keep_all = TRUE),
+              by=by) %>%
+    mutate(source_schema = 'OES_2018_Estimates_Code') %>%
+    select(df_cols)
+
+  by <- setNames('SOC_2010_Code', quo_name(occupation_column))
+
+  df_2010 <- df %>%
+    filter(!(!!occupation_column %in% SOC_Crosswalk$OES_2019_Estimates_Code),
+           !(!!occupation_column %in% SOC_Crosswalk$SOC_2018_Code),
+           !(!!occupation_column %in% SOC_Crosswalk$OES_2018_Estimates_Code),
+           !!occupation_column %in% SOC_Crosswalk$SOC_2010_Code) %>%
+    left_join(SOC_Crosswalk %>% distinct(SOC_2010_Code, .keep_all = TRUE),
+              by=by) %>%
+    mutate(source_schema = 'SOC_2010_Code') %>%
+    select(df_cols)
+
+  df_no_valid_code <- df %>%
+    filter(!(!!occupation_column %in% SOC_Crosswalk$OES_2019_Estimates_Code),
+           !(!!occupation_column %in% SOC_Crosswalk$SOC_2018_Code),
+           !(!!occupation_column %in% SOC_Crosswalk$OES_2018_Estimates_Code),
+           !(!!occupation_column %in% SOC_Crosswalk$SOC_2010_Code)) %>%
+    mutate(crosswalked_code = NA_character_,
+           crosswalked_title = NA_character_,
+           target_schema = soc_schema,
+           source_schema = 'not_valid_detailed_soc') %>%
+    select(df_cols)
+
+  df <- bind_rows(df_2019_estimate,
+                  df_2018,
+                  df_2018_estimate,
+                  df_2010,
+                  df_no_valid_code)
+
+  df <- df %>%
+    mutate(soc_transformed = ifelse(!!occupation_column == crosswalked_code, FALSE, TRUE))
+
+  if (!keep_details) {
+    df <- df %>% select(-c('target_schema','source_schema','soc_transformed'))
+  }
+
+  message('This function will return the occupation code for the first match in the target schema when a SOC code in a source schema is split into more granular target schema occupation codes')
+
+  return(df)
+
+}
+
+
 onet_crosswalk <- function(df, onet_occupation_columnn, onet_soc_schema = 'ONET_SOC_2019_CODE', keep_details = FALSE, include_soc = FALSE) {
 
   onet_occupation_columnn <- enquo(onet_occupation_columnn)
