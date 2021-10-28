e8a9b2b3ea16a422d56e3688165d4d23d1489ddb
Emlyn Bottomley <bottomley.daniel.e@dol.gov>
2021-10-28 11:51:40 -0400

    add data.r file documenting data

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' FIPS codes
+#'
+#' Description TBD
+#'
+#' @format TBD
+#' @source \url TBD
+"FIPS"
+
+#' PIRL Key
+#'
+#' Description TBD
+#'
+#' @format TBD
+#' @source \url TBD
+"PIRL_key"
+
+
+#' PIRL Public
+#'
+#' Description TBD
+#'
+#' @format TBD
+#' @source \url TBD
+"PIRL_public"
