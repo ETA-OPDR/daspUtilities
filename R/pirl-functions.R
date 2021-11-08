125988b14e1dd527007ff5254f4eee96a508917c
Emlyn Bottomley <bottomley.daniel.e@dol.gov>
2021-11-08 13:59:46 -0500

    made stringr call explicit

--- R/pirl-functions.R
+++ R/pirl-functions.R
@@ -4,8 +4,8 @@
 #' as pirl elements and replaces them with friendly names
 #' @export
 pirl_name_replace <- function(df) {
-  pirl_names <- str_replace(names(df), 'PIRL', 'p')
-  pirl_names <- str_replace_all(pirl_names, ' ', "")
-  ordered_colnames <-pirl_key$Variable_Name[match(pirl_names,pirl_key$R_Variable)]
-  colnames(df) <- ordered_colnames
+  pirl_names <- stringr::str_replace(names(df), 'PIRL', 'p')
+  pirl_names <- stringr::str_replace_all(pirl_names, ' ', "")
+  ordered_colnames <-PIRL_key$Variable_Name[match(pirl_names,PIRL_key$R_Variable)]
 }
+
