3039eb30a6e34fe936ebb346a2ec4c4882ae89ba
Emlyn Bottomley <bottomley.daniel.e@dol.gov>
2021-11-15 14:36:55 -0500

    handles unmatched column names

--- R/pirl-functions.R
+++ R/pirl-functions.R
@@ -7,5 +7,7 @@ pirl_name_replace <- function(df) {
   pirl_names <- stringr::str_replace(names(df), 'PIRL', 'p')
   pirl_names <- stringr::str_replace_all(pirl_names, ' ', "")
   ordered_colnames <-PIRL_key$Variable_Name[match(pirl_names,PIRL_key$R_Variable)]
-}
+  ordered_colnames[is.na(ordered_colnames)] <- colnames(df)[is.na(ordered_colnames)]
 
+  return(ordered_colnames)
+}
